bae1f8ba17e1766d8e5cc1d1b949134b43de56f1
Brock Penner <orbithammer@users.noreply.github.com>
2024-01-15 19:32:00 +0900

    removed console.log and comment from Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,9 +3,7 @@ import { Outlet } from "react-router-dom"
 import Header from "/components/Header"
 
 export default function Layout() {
-    // const [response, setResponse] = React.useState("default response")
     const [recommendation, setRecommendation] = React.useState({})
-    console.log("recommendation state layout", recommendation)
     return (
         <>
             <Header />
